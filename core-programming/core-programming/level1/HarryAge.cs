6fc2ad2907dcde8e0b5f07c4ad589d9af92066e5
sandesh <sandeshsri01@gmail.com>
2026-04-28 16:53:03 +0530

    [sandesh Add. Harry Current Age Problem]

---
+++ core-programming/core-programming/level1/HarryAge.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class HarryAge
+{
+    public static void Main(string[] args)
+    {
+       
+        string name = "Harry";
+        int birthYear = 2000;
+        int currentYear = 2024;
+
+      
+        int age = currentYear - birthYear;
+
+       
+        Console.WriteLine(name + "'s age in " + currentYear + " is " + age);
+    }
+}
