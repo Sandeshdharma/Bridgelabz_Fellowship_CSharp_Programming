b64d12e5b1b6c44e200cde9f8c81d35cf91efbee
sandesh <sandeshsri01@gmail.com>
2026-04-28 14:29:48 +0530

    Add AddressBook project files

---
+++ AddressBook/AddressBookSystem.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AddressBook
+{
+    public class AddressBookSystem
+    {
+        private List<Person> contacts = new List<Person>();
+
+        public void AddPerson(Person person)
+        {
+           
+            if (contacts.Any(c => c.Equals(person)))
+            {
+                Console.WriteLine("Error: A person with this name already exists.");
+                return;
+            }
+            contacts.Add(person);
+            Console.WriteLine("Contact added successfully!");
+        }
+
+        public void DisplayAll()
+        {
+            
+            foreach (var contact in contacts.OrderBy(c => c.FirstName))
+            {
+                Console.WriteLine(contact.ToString());
+            }
+        }
+
+        public void SearchByCity(string city)
+        {
+           
+            var results = contacts.Where(c => c.City.Equals(city, StringComparison.OrdinalIgnoreCase)).ToList();
+            if (results.Any())
+                results.ForEach(r => Console.WriteLine(r));
+            else
+                Console.WriteLine("No contacts found in that city.");
+        }
+
+        public void DeletePerson(string firstName)
+        {
+            var person = contacts.FirstOrDefault(c => c.FirstName.Equals(firstName, StringComparison.OrdinalIgnoreCase));
+            if (person != null)
+            {
+                contacts.Remove(person);
+                Console.WriteLine("Contact deleted.");
+            }
+        }
+    }
+}
---
+++ AddressBook/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace AddressBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AddressBookSystem myBook = new AddressBookSystem();
+            bool exit = false;
+
+            while (!exit)
+            {
+                Console.WriteLine("\n--- Address Book Menu ---");
+                Console.WriteLine("1. Add Contact\n2. Display All (Sorted)\n3. Search by City\n4. Delete Contact\n5. Exit");
+                Console.Write("Enter Choice: ");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        Console.Write("First Name: "); string fn = Console.ReadLine();
+                        Console.Write("Last Name: "); string ln = Console.ReadLine();
+                        Console.Write("City: "); string city = Console.ReadLine();
+                        Console.Write("State: "); string state = Console.ReadLine();
+                        myBook.AddPerson(new Person(fn, ln, city, state));
+                        break;
+                    case "2":
+                        myBook.DisplayAll();
+                        break;
+                    case "3":
+                        Console.Write("Enter City: ");
+                        myBook.SearchByCity(Console.ReadLine());
+                        break;
+                    case "4":
+                        Console.Write("Enter First Name to Delete: ");
+                        myBook.DeletePerson(Console.ReadLine());
+                        break;
+                    case "5":
+                        exit = true;
+                        break;
+                }
+            }
+        }
+    }
+}
