283e4ada3a19f0768e5ebb4d567eb2e1b3521b52
sandesh <sandeshsri01@gmail.com>
2026-04-28 16:14:46 +0530

    [sandesh Add. Marks/Result Program Basic]

---
+++ core-programming/core-programming/level1/SamMarks.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class DisplayResult
+{
+    public static void Main(string[] args)
+    {
+
+   
+        string name = "Sam";
+        int rollNumber = 1;
+
+      
+        double percentMarks = 99.99;
+
+    
+        char result = 'P';
+
+        Console.WriteLine(result);
+
+}
+}
