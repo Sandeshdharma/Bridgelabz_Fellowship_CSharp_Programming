f7a0267558058b35cd7b8cc755a9ad33029d33f0
sandesh <sandeshsri01@gmail.com>
2026-04-28 14:51:14 +0530

    add file

---
+++ CSharp_Oops_Programming/review/AddressBook/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AddressBook
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+
+        public Person(string firstName, string lastName, string city, string state)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            City = city;
+            State = state;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {FirstName} {LastName}, City: {City}, State: {State}";
+        }
+
+      
+        public override bool Equals(object obj)
+        {
+            if (obj is Person other)
+            {
+                return FirstName.Equals(other.FirstName, StringComparison.OrdinalIgnoreCase) &&
+                       LastName.Equals(other.LastName, StringComparison.OrdinalIgnoreCase);
+            }
+            return false;
+        }
+
+        public override int GetHashCode() => (FirstName + LastName).ToLower().GetHashCode();
+    }
+}
