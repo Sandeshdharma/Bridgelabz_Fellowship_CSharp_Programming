c62e7a06fbaf23924660f297c0897ece7be7fec2
sandesh <sandeshsri01@gmail.com>
2026-05-14 16:49:15 +0530

    [sandesh.add Annotation Obsolete demoprogram]

---
+++ IO Stream/Annotation/Annotation/Annotation-Obsolete-Demo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace AnnotationDemo
+{
+   
+    public class oldApi
+    {
+      
+        [Obsolete("dont use this is old")]
+        public void OldFeature()
+        {
+            Console.WriteLine("Executing Old Feature logic");
+        }
+
+        public void NewFeature()
+        {
+            Console.WriteLine("Executing New, Optimized Feature logic!");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            oldApi api = new oldApi();
+
+           
+            Console.WriteLine("--- Calling New Feature ---");
+            api.NewFeature();
+
+            Console.WriteLine("\n--- Calling Old Feature ---");
+            api.OldFeature(); 
+
+            Console.ReadLine();
+        }
+    }
+}
