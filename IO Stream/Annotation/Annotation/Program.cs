9d469e574835825f9d09ff885990a1bf4ab133e7
sandesh <sandeshsri01@gmail.com>
2026-05-22 10:56:30 +0530

    [Sandesh.Add/Updated Program.cs(Annotation-obsolete-Demo)]

---
+++ IO Stream/Annotation/Annotation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Annotation
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Obsolete Ob = new Obsolete();
+            Ob.oldMethod();
+            Console.ReadLine();
+        }
+    }
+}
