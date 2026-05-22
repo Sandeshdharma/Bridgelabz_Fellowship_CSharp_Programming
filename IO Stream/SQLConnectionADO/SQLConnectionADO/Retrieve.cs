30a934bf77daa051293b8e186e374f4e4464bda7
sandesh <sandeshsri01@gmail.com>
2026-05-22 12:54:58 +0530

    [Sandesh.add Retrieve Operation]

---
+++ IO Stream/SQLConnectionADO/SQLConnectionADO/Retrieve.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Data.SqlClient;
+using System.Globalization;
+
+namespace SQLConnectionADO
+{
+     class Retrieve
+    {
+        public void ViewData()
+        {
+            SqlConnection con = new SqlConnection("Server=localhost;Database=Student_Details;Integrated Security = True; TrustServerCertificate = True");
+            String query = "select * from Student";
+            SqlCommand cmd = new SqlCommand(query , con);
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine($"Name: {reader["Name"]}, Roll: {reader["RollNumber"]}, Email: {reader["Email"]}, Phone: {reader["PhoneNumber"]}");
+            }
+            con.Close();
+            Console.WriteLine("data fetch sucessfully");
+
+
+
+        }
+    }
+}
