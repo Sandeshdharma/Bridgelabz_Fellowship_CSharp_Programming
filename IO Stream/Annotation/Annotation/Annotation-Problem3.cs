cd7506e7dad9f7f7b6f5636b74298b768c3ea671
sandesh <sandeshsri01@gmail.com>
2026-05-22 11:12:02 +0530

    [Sandesh.Add Annotaion-Problem3]

---
+++ IO Stream/Annotation/Annotation/Annotation-Problem3.cs
@@ -0,0 +1,30 @@
+﻿//using Annotation;
+//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace Annotation
+//{
+//    internal class Annotation_Problem3
+//    {
+//        [Obsolete("use Add(list<int> Number) method" , true)]
+//        public static int Add(int FirstNumber, int SecondNumber)
+//        {
+//            return FirstNumber + SecondNumber;
+//        }
+//        public static int Add(List<int> Numbers)
+//        {
+//            int sum = 0;
+//            foreach( int Number in Numbers)
+//            {
+//                sum = sum + Number;
+
+//            }
+//            return sum;
+//        }
+      
+
+//    }
+
+//}
+
