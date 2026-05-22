125a91550a86d715eea609a02bfa8b6c05d899ca
sandesh <sandeshsri01@gmail.com>
2026-05-22 12:55:54 +0530

    [Sandesh.add Read Operation]

---
+++ IO Stream/SQLConnectionADO/SQLConnectionADO/ReadNew.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+
+
+namespace SQLConnectionADO
+{
+     class ReadNew
+    {
+
+        public void Read()
+        {
+            SqlConnection con = new SqlConnection("server=localhost; database= StudentInfo; Integrated Security= True; TrustServerCertificate= True");
+            string query = "Select * from StudentData";
+            SqlCommand cmd = new SqlCommand(query, con);
+
+            con.Open();
+            SqlDataReader Reader = cmd.ExecuteReader();
+
+            while (Reader.Read())
+            {
+                Console.WriteLine(
+                    Reader["Id"] + " " +
+                    Reader["Name"] + " " +
+                    Reader["Section"] + " " +
+                    Reader["PhoneNo"] + " ");
+
+
+
+
+                    
+
+
+            }
+            con.Close();
+
+
+
+        }
+    }
+}
+
