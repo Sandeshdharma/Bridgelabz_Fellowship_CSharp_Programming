a9d5fbe4aadafa0c962a1f20e384b21bb37ccb3e
sandesh <sandeshsri01@gmail.com>
2026-05-22 11:42:47 +0530

    [Sandesh.Add Custom Annotation Program]

---
+++ IO Stream/Annotation/Annotation/CustomAttri.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+
+namespace Annotation
+{
+
+    [AttributeUsage(AttributeTargets.Method)]
+    public class StatusAttribute : Attribute
+    {
+        public string Stage { get; }
+
+        public StatusAttribute(string stage)
+        {
+            Stage = stage;
+        }
+    }
+
+
+    public class FeatureManager
+    {
+        [Status("Stable")]
+        public void LegacyBillingSystem()
+        {
+            Console.WriteLine("Running old reliable billing...");
+        }
+
+        [Status("Experimental")]
+        public void CryptoPaymentBeta()
+        {
+            Console.WriteLine("Running risky beta feature...");
+        }
+    }
+}
