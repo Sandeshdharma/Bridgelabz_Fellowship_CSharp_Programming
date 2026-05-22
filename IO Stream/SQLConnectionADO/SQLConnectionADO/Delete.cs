2ca0805f1dd49803d6f7e4c777058e3cda842b2f
sandesh <sandeshsri01@gmail.com>
2026-05-22 12:57:38 +0530

    [Sandesh.add delete]

---
+++ IO Stream/SQLConnectionADO/SQLConnectionADO/Delete.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Data.SqlClient;
+
+namespace SQLConnectionADO
+{
+    class Delete
+    {
+
+
+        public void DeleteData()
+        {
+            SqlConnection con = new SqlConnection("Server = localhost; Database= Student_Details; Integrated Security = True; TrustServerCertificate= True");
+
+            string query = "delete from Student where RollNumber ='57'";
+
+            SqlCommand cmd = new SqlCommand(query, con);
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+            Console.WriteLine("Record Deleted Successfully");
+
+
+
+        }
+    }
+}
