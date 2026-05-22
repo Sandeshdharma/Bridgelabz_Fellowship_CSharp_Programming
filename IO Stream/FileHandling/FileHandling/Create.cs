f127447d7e405ab0f99859263aef5e1be240f3c6
sandesh <sandeshsri01@gmail.com>
2026-05-22 11:49:13 +0530

    [Sandesh. Add Create file using File Stream]

---
+++ IO Stream/FileHandling/FileHandling/Create.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileHandling
+{
+     class Create
+    {
+
+        public void createfile()
+        {
+
+            string path = @"C:\Users\91638\Desktop\C_Sharp_Dot_Net\IO Stream\FileHandling\File2.txt";
+
+            FileStream fs = new FileStream(path, FileMode.Create);
+
+            fs.Close();
+            Console.WriteLine("created");
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
