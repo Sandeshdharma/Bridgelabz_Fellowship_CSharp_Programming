22570206fc926b96d1f1cbd1c218c251c8dab5cf
sandesh <sandeshsri01@gmail.com>
2026-05-22 12:01:49 +0530

    [Sandesh.Add ReadFile(using File.exit method also)]

---
+++ IO Stream/FileHandling/FileHandling/Read.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace FileHandling
+{
+     class Read
+    {
+        public void ReadContent()
+        {
+            if (File.Exists("data.txt"))
+            {
+                string content = File.ReadAllText("data.txt");
+                Console.WriteLine(content);
+            }
+            else
+            {
+                Console.WriteLine("invalid");
+            }
+        }
+     
+    }
+}
