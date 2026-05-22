84b36e47c2b3ea26b179517c9e2fdcab5f93d58f
sandesh <sandeshsri01@gmail.com>
2026-05-22 12:52:45 +0530

    [Sandesh.add Insert Operation]

---
+++ IO Stream/SQLConnectionADO/SQLConnectionADO/Insert.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace SQLConnectionADO
+{
+    class Insert
+    {
+        public void insertdata()
+        {
+            SqlConnection con = new SqlConnection("Server= localhost; Database = Student_Details; Integrated Security=True; TrustServerCertificate= True");
+
+            string query = "insert into Student(Name, RollNumber, Email, PhoneNumber) values ('Sandesh', '57', 'sandeshsri01@gmail.com', '6388694107'), ('Tushar', '58', 'tushar12@gmail.com', '9307827538')";
+
+            SqlCommand cmd = new SqlCommand(query, con);
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            Console.WriteLine("inserted success");
+        }
+    }
+}
