1b9190d25ecdf2900f9e5c6811d3cc32b91e3417
sandesh <sandeshsri01@gmail.com>
2026-05-22 12:41:51 +0530

    [Sandesh.add Start Delegates Thread]

---
+++ IO Stream/Multithreading/StartThreadDelegates.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Multithreading
+{
+     class StartThreadDelegates
+    {
+
+        public void StartThd()
+        {
+            ThreadStart obj = new ThreadStart(printnumber);
+            Thread T1 = new Thread(obj);
+            T1.Start();
+
+          
+        }
+        public void printnumber()
+        {
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("numbers:" + i);
+            }
+        }
+    }
+}
