189d784f728707349acae61b3dd6c73a2e39e0f3
sandesh <sandeshsri01@gmail.com>
2026-05-22 12:29:24 +0530

    [Sandesh.add Fileinfo]

---
+++ IO Stream/FileHandling/FileHandling/FileInformation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+
+namespace FileHandling
+{
+     class FileInformation
+    {
+        public void FileCompleteDetails()
+        {
+            FileInfo file = new FileInfo("data.txt");
+            Console.WriteLine(file.Length);
+            Console.WriteLine(file.Name);
+            Console.WriteLine(file.CreationTime);
+
+           
+        }
+    }
+}
