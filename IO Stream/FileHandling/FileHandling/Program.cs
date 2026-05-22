defc82e0599bf2e8efe8e6ae94262ace4b7318e9
sandesh <sandeshsri01@gmail.com>
2026-05-22 11:52:15 +0530

    [Sandesh.Add Program.cs file (Create)]

---
+++ IO Stream/FileHandling/FileHandling/Program.cs
@@ -0,0 +1,102 @@
+﻿
+using System;
+using System.IO;
+namespace FileHandling
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Read fileReader = new Read();
+            //fileReader.ReadContent();
+
+            //Console.ReadLine();
+
+            //Append fileAdd = new Append();
+            //fileAdd.Appenddata();
+            //Console.ReadLine();
+
+            //WriteMultiple addlines = new WriteMultiple();
+            //addlines.WriteMultipleLines();
+
+            //Console.ReadLine();
+
+            //FileInformation f = new FileInformation();
+            //f.FileCompleteDetails();
+            //Console.ReadLine();
+
+
+            //DirectoryCreation dc = new DirectoryCreation();
+            //dc.CreateDirectory();
+            //Console.ReadLine();
+
+            //CreateUsingStream filecreate = new CreateUsingStream();
+            //filecreate.CreateFile();
+            //Console.ReadLine();
+
+
+            //WriteUsingStream ws = new WriteUsingStream();
+            //ws.Write();
+            //Console.ReadLine();
+
+            //AppendUsingFileStream apnd = new AppendUsingFileStream();
+            //apnd.Appendtext();
+            //Console.ReadLine();
+
+
+            //Append2 apd = new Append2();
+            //apd.newAppend();
+            //Console.ReadLine();
+
+            //ReadusingFilestream rfs = new ReadusingFilestream();
+            //rfs.Reading();
+            //Console.ReadLine();
+
+
+            //Create cs = new Create();
+            //cs.createfile();
+            //Console.ReadLine();
+
+            //WriteUsingStream2 wrd = new WriteUsingStream2();
+            //wrd.write();
+            //Console.ReadLine();
+
+
+
+            //ReadUsingStreamReader1 read = new ReadUsingStreamReader1();
+            //read.Read();
+            //Console.ReadLine();
+
+
+            //WriteUsingStreamWriter wr = new WriteUsingStreamWriter();
+            //wr.Write();
+            //Console.ReadLine();
+
+
+            //CreateUsingTextWriter CT =  new CreateUsingTextWriter();
+            //CT.Create();
+            //Console.ReadLine();
+
+            //FileCreateUsing FL = new FileCreateUsing();
+            //FL.CreateFile();
+            //Console.ReadLine();
+
+
+            //AppendFile Ap = new AppendFile();
+            //Ap.Apnd();
+            //Console.ReadLine();
+
+            //WriteusingFileStream WTF =  new WriteusingFileStream();
+
+            //WTF.WriteTxt();
+            //Console.ReadLine();
+
+
+            Read rs = new Read();
+            rs.ReadContent();
+            Console.ReadLine();
+
+        }
+    }
+
+}
