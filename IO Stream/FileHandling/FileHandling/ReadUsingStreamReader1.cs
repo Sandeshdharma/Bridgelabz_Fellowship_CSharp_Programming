5a91e55bbcf4324cba9297132b456b56c3e96f5a
sandesh <sandeshsri01@gmail.com>
2026-05-22 12:04:00 +0530

    [Sandesh. add Read using Stream Reader]

---
+++ IO Stream/FileHandling/FileHandling/ReadUsingStreamReader1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileHandling
+{
+    class ReadUsingStreamReader1
+    {
+
+        public void Read()
+        {
+            string path = @"C:\Users\91638\Desktop\C_Sharp_Dot_Net\IO Stream\FileHandling\File1.txt";
+            string data;
+
+            FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+
+            using (StreamReader sr = new StreamReader(fs))
+            {
+
+                data = sr.ReadToEnd();
+
+
+            }
+            Console.WriteLine(data);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
