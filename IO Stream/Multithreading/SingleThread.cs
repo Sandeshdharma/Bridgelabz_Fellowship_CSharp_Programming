28ee6572bb008265a94afcdd11201c06e83ffcc9
sandesh <sandeshsri01@gmail.com>
2026-05-22 12:39:19 +0530

    [Sandesh.add single Thread]

---
+++ IO Stream/Multithreading/SingleThread.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Multithreading
+{
+    internal class SingleThread
+    {
+        public void Method_slp1()
+        {
+
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Method1" + i);
+            }
+
+        }
+        public void Method_slp2()
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("method2" + i);
+            }
+        }
+        public void Method_slp3()
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("method3" + i);
+            }
+        }
+
+    }
+}
