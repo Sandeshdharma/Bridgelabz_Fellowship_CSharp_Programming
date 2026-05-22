08f944eaa4b9d9757705435d3f5d7752df99f9eb
sandesh <sandeshsri01@gmail.com>
2026-05-22 12:14:12 +0530

    [Sandesh.Add Create using text writer]

---
+++ IO Stream/FileHandling/FileHandling/CreateUsingTextWriter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileHandling
+{
+    class CreateUsingTextWriter
+    {
+
+        public void Create()
+        {
+            string path = @"C:\Users\91638\Desktop\C_Sharp_Dot_Net\IO Stream\FileHandling\TextFile.txt";
+            using(TextWriter TW = File.CreateText(path))
+            {
+                TW.WriteLine("hello");
+                TW.WriteLine("done");
+            }
+            Console.WriteLine("succesfully");
+        }
+
+    }
+}
