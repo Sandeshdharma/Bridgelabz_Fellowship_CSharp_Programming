9aaed30e1785a74294044af65b73ad1e84876785
sandesh <sandeshsri01@gmail.com>
2026-05-22 12:05:30 +0530

    [Sandesh.Add Append file ]

---
+++ IO Stream/FileHandling/FileHandling/Append.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+
+namespace FileHandling
+{
+    class Append
+    {
+        public void Appenddata()
+        {
+
+            if (File.Exists("data.txt"))
+            {
+                File.AppendAllText("data.txt", "new lines append in the file data");
+                Console.WriteLine("Appended");
+
+            }
+            else
+            {
+                Console.WriteLine("file not found");
+            }
+          
+            
+
+        }
+
+    
+    }
+}
