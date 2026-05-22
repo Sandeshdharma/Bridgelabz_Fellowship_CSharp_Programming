b509d7f9523dec411382eb36ee71c3393741d61f
sandesh <sandeshsri01@gmail.com>
2026-05-22 12:53:57 +0530

    [Sandesh.add Update Operation]

---
+++ IO Stream/SQLConnectionADO/SQLConnectionADO/Update.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace SQLConnectionADO
+{
+     class Update
+    {
+
+        public void UpdateData()
+        {
+            SqlConnection con = new SqlConnection("server=localhost; database= StudentInfo; Integrated Security= True; TrustServerCertificate= True");
+            string query = "UPDATE StudentData  SET ID= '4' where Name ='anshika'";
+            SqlCommand cmd = new SqlCommand(query, con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            Console.WriteLine("Data updated");
+            con.Close();
+
+        }
+    }
+}
