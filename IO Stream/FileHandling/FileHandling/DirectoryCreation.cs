c3e1b0c316584ff02dbc970a78264393cb09402c
sandesh <sandeshsri01@gmail.com>
2026-05-22 12:27:20 +0530

    [Sandesh.add Directotry creation]

---
+++ IO Stream/FileHandling/FileHandling/DirectoryCreation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+
+namespace FileHandling
+{
+     class DirectoryCreation
+    {
+
+        public void CreateDirectory()
+        {
+            if (Directory.Exists("Sandesh-Data"))
+            {
+                Console.WriteLine("exist folder");
+            }
+            else
+            {
+                Console.WriteLine("not exiswt");
+            }
+            
+        }
+    }
+}
