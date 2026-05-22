01fbbac84c670a6aa020d74efbfdae249a15cadb
sandesh <sandeshsri01@gmail.com>
2026-05-22 12:32:51 +0530

    [Sandesh.Add Deadlock]

---
+++ IO Stream/Multithreading/Deadlock.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Multithreading
+{
+    internal class Deadlock
+    {
+        static readonly object lock1 = new object();
+        static readonly object lock2 = new object();
+        public void thread1()
+        {
+            lock(lock1)
+            {
+                Console.WriteLine("thread 1 acquire lock1");
+                Thread.Sleep(5000);
+                lock (lock2)
+                {
+                    Console.WriteLine("thread 1 acquire lock2");
+                }
+            }
+        }
+        public void thread2()
+        {
+            lock (lock1)
+            {
+                Console.WriteLine("thread 2 acquire lock2");
+                Thread.Sleep(5000);
+                lock (lock2)
+                {
+                    Console.WriteLine("thread 2 acquire lock1");
+                }
+            }
+        }
+
+
+    }
+}
